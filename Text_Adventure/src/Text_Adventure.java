db1f3d81f477ccba352594beb997af57359df55e
LordX1233 <sebastian@varillas.net>
2023-11-20 18:22:34 -0400

    Cool thing
    very cool 😎

--- Text_Adventure/src/Text_Adventure.java
+++ Text_Adventure/src/Text_Adventure.java
@@ -1,8 +1,50 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Text_Adventure {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Prueba");
         System.out.println("soy ethan");
         System.out.println("ciwebnfebfiuewybfiyeuwbfuewbf tu mama");
+
+        Scanner scan = new Scanner(System.in);
+        try {
+            try {
+                FileWriter deleteContent = new FileWriter("file.txt", false);
+                deleteContent.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            FileWriter writer = new FileWriter("file.txt", true);
+
+            System.out.println("Your name: ");
+            String name = scan.nextLine();
+
+            writer.write("Your name: " + name + "\n");
+
+            writer.write("\n");
+            writer.write("-------------------------------------------------------\n");                             
+            writer.write("░░██████░░░░░░░░██████░░\n");                                   
+            writer.write("░░░░░░░░░░░░░░░░░░░░░░░░\n");                  
+            writer.write("░░░░████░░░░░░░░████░░░░            Health: 20\n");                  
+            writer.write("░░░░████░░░░░░░░████░░░░\n");                  
+            writer.write("░░░░░░░░░░░░░░░░░░░░░░░░\n");                  
+            writer.write("░░░░░░░░░░░░░░░░░░░░░░░░\n");                  
+            writer.write("░░░░██░░░░░░░░░░░░██░░░░\n");                  
+            writer.write("░░░░░░██░░░░░░░░██░░░░░░\n");                  
+            writer.write("░░░░░░░░████████░░░░░░░░\n");      
+            writer.write("-------------------------------------------------------\n");                    
+        
+            writer.close();
+
+            System.out.println("Text written to file successfully.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        scan.close();
     }
 }
