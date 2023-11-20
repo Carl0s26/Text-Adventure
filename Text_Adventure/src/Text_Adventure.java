31f2505076ba9ce0213278975f6194a2ce2b45b2
Carl0s26 <Carloskepp26@gmail.com>
2023-11-20 17:56:10 -0400

    prueba para Ethan
    test: )
    Co-Authored-By: Samuray360 <151565282+samuray360@users.noreply.github.com>

--- Text_Adventure/src/Text_Adventure.java
+++ Text_Adventure/src/Text_Adventure.java
@@ -1,5 +1,6 @@
-public class App {
+public class Text_Adventure {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Prueba");
     }
 }
